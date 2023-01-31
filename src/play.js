b58325edbfa83b557b4cb0262b93d9c901e94bbc
Raman-RKB <121190700+Raman-RKB@users.noreply.github.com>
2023-01-31 13:02:13 +0300

    first commit

---
+++ src/play.js
@@ -0,0 +1,183 @@
+//debugger;
+class Game {
+    constructor(parent, cardSet) {
+        this.parent = parent;
+        this.level = level;
+        this.cardSet = cardSet;
+
+        this.setPairCard = []
+        this.setArr = []
+        this.element = templateEngine(Game.startPlayTemplate());
+        parent.appendChild(this.element);
+
+        this.timerMinutes = this.element.querySelector(".timer__minutes");
+        this.timerSeconds = this.element.querySelector(".timer__seconds");
+
+        this.onStartTimer.bind(this);
+        this.onStartTimer();
+        this.onDestroyTimer.bind(this);
+
+        this.gameHeaderRestartButton = this.element.querySelector(".game__header_restart-button");
+
+        this.onRenderGameInterface.bind(this);
+        this.onRenderGameInterface();
+
+        this.onShowCards = this.onShowCards.bind(this);
+
+        this.onCheckMatch = this.onCheckMatch.bind(this);
+
+        this.gameHeaderRestartButton.addEventListener("click", this.onRestartGameClick.bind(this));
+    }
+
+    onRenderGameInterface() {
+        this.cardSet.forEach(el => {
+            this.cardSetItem = templateEngine(Game.cardSetItemTemplate(el));
+            this.element.appendChild(this.cardSetItem);
+        });
+
+        if (this.cardSet.length === 6) {
+            this.element.classList.add('game-row-adapting-if-6-cards');
+            const gameHeader = this.element.querySelector(".game__header")
+            gameHeader.classList.add('game__header-span-adapting-if-6-cards');
+        }
+
+        if (this.cardSet.length === 12) {
+            this.element.childNodes[this.element.childNodes.length - 3].classList.add('second-row-centering-if-12-cards');
+
+            const gameHeaderMinPointer = this.element.querySelector(".game__header_min-pointer")
+            gameHeaderMinPointer.classList.add('game__header_min-display-adapting-if-12-cards');
+
+            const gameHeaderSinPointer = this.element.querySelector(".game__header_sec-pointer")
+            gameHeaderSinPointer.classList.add('game__header_sec-display-adapting-if-12-cards');
+        }
+
+        if (this.cardSet.length === 18) {
+
+            const gameHeaderMinPointer = this.element.querySelector(".game__header_min-pointer")
+            gameHeaderMinPointer.classList.add('game__header_min-display-adapting-if-12-cards');
+
+            const gameHeaderSinPointer = this.element.querySelector(".game__header_sec-pointer")
+            gameHeaderSinPointer.classList.add('game__header_sec-display-adapting-if-12-cards');
+        }
+        setTimeout(this.onHideCards.bind(this), 1000);
+    }
+
+    onRestartGameClick() {
+        this.element.remove()
+        level = new Level(document.querySelector(".body"));
+    }
+
+    onCheckMatch() {
+
+        if (this.setPairCard[0].id === this.setPairCard[1].id) {
+
+        } else {
+            const losePopup = new Popup(this.parent, 'lose', `${this.timerMinutes.textContent}.${this.timerSeconds.textContent}`);
+            this.onDestroyTimer()
+        }
+
+        this.setPairCard = [];
+    }
+
+    onShowCards(event) {
+        const target = event.target;
+        target.style.backgroundImage = null
+
+        if (this.setPairCard.length === 0) {
+            this.setArr.push(target);
+            this.setPairCard.push(target);
+
+        } else if (this.setPairCard.length > 0 && this.setArr.length < this.cardSet.length - 1) {
+            this.setArr.push(target);
+            this.setPairCard.push(target);
+            this.onCheckMatch();
+        } else {
+            const winPopup = new Popup(this.parent, 'win', `${this.timerMinutes.textContent}.${this.timerSeconds.textContent}`);
+            this.onDestroyTimer()
+        }
+    }
+
+    onHideCards() {
+        for (let i = 1; i <= this.cardSet.length; i++) {
+            this.element.childNodes[this.element.childNodes.length - i].style.backgroundImage = "url(./src/img/face.jpg)";
+            this.element.addEventListener("click", this.onShowCards);
+        }
+    }
+
+    onStartTimer() {
+        this.timerMinutes.setAttribute("data-minutes", "");
+        this.timerSeconds.setAttribute("data-seconds", "");
+
+        const timer = timezz(document.querySelector('.timer'), {
+            date: new Date(),
+        });
+
+        timer.stopOnZero = false;
+    }
+
+    onDestroyTimer() {
+        const timer = timezz(document.querySelector('.timer'), {
+            date: new Date(),
+        });
+
+        timer.destroy();
+    }
+
+}
+
+Game.cardSetItemTemplate = (el) => ({
+    tag: 'div',
+    cls: ['game__container_item', `game__container_item-${el}`],
+    attrs: {
+        id: `${el}`,
+    }
+});
+
+Game.startPlayTemplate = () => ({
+    tag: 'section',
+    cls: 'game',
+    content: [
+        {
+            tag: 'div',
+            cls: 'game__header',
+            content: [
+                {
+                    tag: 'div',
+                    cls: 'timer',
+                    content: [
+                        {
+                            tag: 'div',
+                            cls: 'timer__minutes',
+                        },
+                        {
+                            tag: 'div',
+                            //cls: 'timer__minutes',
+                            content: '.'
+                        },
+                        {
+                            tag: 'div',
+                            cls: 'timer__seconds',
+                            //content: '.'
+                        }
+                    ],
+                },
+                {
+                    tag: 'div',
+                    cls: 'game__header_min-pointer',
+                    content: 'min'
+                },
+                {
+                    tag: 'div',
+                    cls: 'game__header_sec-pointer',
+                    content: 'sek'
+                },
+                {
+                    tag: 'button',
+                    cls: 'game__header_restart-button',
+                    content: 'Начать заново'
+                },
+            ],
+        },
+    ],
+});
+
---
+++ src/popup.js
@@ -0,0 +1,77 @@
+class Popup {
+    constructor(body, result, stopwatch) {
+        this.parent = body;
+        this.result = result;
+        this.stopwatch = stopwatch;
+
+        this.popupBackground = templateEngine(Popup.PopupBackgroundTemplate());
+        this.parent.appendChild(this.popupBackground);
+
+        this.renderPopup.bind(this);
+        this.renderPopup();
+
+
+        this.popupPlayAgainButton = this.element.querySelector('.popup_play-again-button');
+        this.onRestartGameClick.bind(this)
+        this.popupPlayAgainButton.addEventListener("click", this.onRestartGameClick.bind(this));
+    }
+
+    onRestartGameClick() {
+
+        this.parent.replaceChildren()
+        level = new Level(document.querySelector(".body"));
+    }
+
+    renderPopup() {
+        if (this.result === 'lose') {
+
+            this.element = templateEngine(Popup.PopupTemplate(this.result, 'Вы проиграли!', this.stopwatch));
+            this.parent.appendChild(this.element);
+
+        } else if (this.result === 'win') {
+
+            this.element = templateEngine(Popup.PopupTemplate(this.result, 'Вы выиграли!', this.stopwatch));
+            this.parent.appendChild(this.element);
+        }
+    }
+
+}
+
+Popup.PopupBackgroundTemplate = () => ({
+    tag: 'section',
+    cls: 'result-background',
+});
+
+Popup.PopupTemplate = (resultTag, resultText, passedTime) => ({
+    tag: 'div',
+    cls: `${resultTag}`,
+    content: [
+        {
+            tag: 'img',
+            cls: `${resultTag}__popup_icon`,
+            attrs: {
+                src: `./src/img/${resultTag}icon.svg`,
+            }
+        },
+        {
+            tag: 'div',
+            cls: `${resultTag}__popup_title`,
+            content: resultText
+        },
+        {
+            tag: 'div',
+            cls: `${resultTag}__popup_stopwath-title`,
+            content: 'Затраченное время:'
+        },
+        {
+            tag: 'div',
+            cls: `${resultTag}__popup_stopwath-display`,
+            content: `${passedTime}`
+        },
+        {
+            tag: 'button',
+            cls: ['popup_play-again-button', `${resultTag}__popup_play-again-button`],
+            content: 'Играть снова'
+        },
+    ],
+});
